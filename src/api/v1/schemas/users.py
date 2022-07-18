49f8c8a9e23fc59140a89ba96d3e07fe7b6fbff4
SadLaboka <jitubuh@gmail.com>
2022-07-18 20:11:22 +0300

    add custom validation

--- src/api/v1/schemas/users.py
+++ src/api/v1/schemas/users.py
@@ -1,8 +1,9 @@
+import re
 import uuid as uuid_pkg
 
 from datetime import datetime
 
-from pydantic import BaseModel
+from pydantic import BaseModel, Field, validator
 
 __all__ = (
     "Token",
@@ -19,8 +20,7 @@ class Token(BaseModel):
 
 
 class UserBase(BaseModel):
-    username: str
-    email: str
+    username: str = Field(min_length=4, max_length=20)
 
 
 class UserLogin(BaseModel):
@@ -29,16 +29,37 @@ class UserLogin(BaseModel):
 
 
 class UserCreate(UserBase):
-    password: str
+    password: str = Field(min_length=4, max_length=30)
+    email: str = Field(min_length=6, max_length=25)
+
+    @validator("email")
+    def check_email(cls, v):
+        pattern = r"^[-\w\.]+@([-\w]+\.)+[-\w]{2,4}$"
+
+        if not re.match(pattern, v):
+            raise ValueError('Uncorrect email')
+        return v
 
 
 class UserModel(UserBase):
     uuid: uuid_pkg.UUID
+    email: str
     created_at: datetime
     is_superuser: bool
     is_active: bool
 
 
 class UserUpdate(BaseModel):
-    username: str = None
-    email: str = None
+    username: str = Field(default=None, min_length=4, max_length=20)
+    email: str = Field(default=None, min_length=6, max_length=25)
+
+    @validator("email")
+    def check_email(cls, v):
+        pattern = r"^[-\w\.]+@([-\w]+\.)+[-\w]{2,4}$"
+
+        if v is None:
+            return v
+
+        if not re.match(pattern, v):
+            raise ValueError('Uncorrect email')
+        return v
